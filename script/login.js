1ab6e314ded3359646e84033779704d53b2a452e
ashikur-rahman-jim <ashikurrahman50660@gmail.com>
2026-03-06 21:07:17 -0500

    Designed sign-in page and added functionalities

---
+++ script/login.js
@@ -0,0 +1,22 @@
+document.getElementById('signin-btn').addEventListener('click', function () {
+    // console.log('Sign-in button clicked');
+
+    // Get input-username value
+    const userName = document.getElementById('input-username');
+    const userNameValue = userName.value;
+    // console.log(userNameValue);
+
+    // Get input-password value
+    const userPassword = document.getElementById('input-password');
+    const passwordValue = userPassword.value;
+    console.log(passwordValue);
+
+    // if username and password is currect sign-in successfull else sign-in faild
+    if(userNameValue === "admin" && passwordValue === "admin123") {
+        alert('Sign in Successfull');
+        window.location.assign('/home.html');
+    } else {
+        alert('Sign in Faild');
+    }
+    
+})
